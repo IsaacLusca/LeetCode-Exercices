706da814989a4fa3e2d4eadf414dd199033a6e31
Isaac Lucas <90662483+IsaacLusca@users.noreply.github.com>
2024-10-23 15:05:34 -0300

    Time: 0 ms (100%), Space: 8.5 MB (17.81%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -0,0 +1,62 @@
+typedef struct no{
+    char elem;
+    struct pilha *prox;
+}No;
+
+No *add(No *topo, char caract)
+{
+    No *novo = malloc(sizeof(No));
+    if(novo == NULL)
+    {
+        return NULL;
+    }
+    novo->elem = caract;
+    novo->prox = topo;
+    topo = novo;
+
+    return topo;
+}
+
+No *rem(No *topo)
+{
+    if (topo == NULL)
+    {
+        printf("Sem valor para desempilhar/n");
+        return NULL;
+    }
+    No *remover = topo;
+    topo = topo->prox;
+    free(remover);
+    return topo;
+}
+
+bool isValid(char* s) {
+    No *topo = NULL;
+    int n = strlen(s);
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+        {
+            topo = add(topo, s[i]);
+        }
+        else if(s[i] == ')' || s[i] == ']' || s[i] == '}')
+        {
+            if (topo == NULL)
+            {
+                return false;
+            }
+            char topChar = topo->elem;
+            topo = rem(topo);
+            if (s[i] == ')' && topChar != '(' ||
+                s[i] == ']' && topChar != '[' ||
+                s[i] == '}' && topChar != '{'
+            )
+            {
+                return false;
+            }
+        }
+        i++;
+    }
+    return topo == NULL; 
+}
